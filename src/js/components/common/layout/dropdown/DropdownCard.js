56dde347ea0660680b6321b31208c992eac630a0
maxim_novikov_88 <m.v.novikov@gmail.com>
2019-01-24 10:51:11 +0300

    rewrite dropdown card with hooks

--- src/js/components/common/layout/dropdown/DropdownCard.js
+++ src/js/components/common/layout/dropdown/DropdownCard.js
@@ -1,100 +1,81 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import DropdownCardItem from "./DropdownCardItem";
 
-export default class DropdownCard extends Component{
+export default function DropdownCard(props){
 
-    state = {
-        showMenu: false,
-        value: this.props.value
-    };
+    const [showMenu, setShowMenu] = useState(false);
+    const [value, setValue] = useState(props.value);
 
-    componentWillMount(){
-        if(!this.props.value && this.props["dropdownArrItems"]){
-            this.setState({
-                value: this.props["dropdownArrItems"][0]
-            })
+    let dropdownMenu;
+
+    useEffect(() => {
+        if(!props.value && props["dropdownArrItems"]){
+            setValue(props["dropdownArrItems"][0])
         }
-    }
 
-    showBtnHandler = (e) => {
+        document.addEventListener("click", closeMenu);
+        document.addEventListener("scroll", closeMenu);
+    });
+
+
+    function showBtnHandler(e){
         e.preventDefault();
 
-        this.setState(
-            () => {
-                return {
-                    showMenu: true
-                };
-            },
-            () => {
-                document.addEventListener("click", this.closeMenu);
-                document.addEventListener("scroll", this.closeMenu);
-            }
-        )
+        setShowMenu(true, () => {
+            console.log(setShowMenu);
+        });
 
-    };
-
-    closeMenu = (e, target) => {
-        const eventTarget = (e ? e.target : target);
-        if (this.dropdownMenu && !this.dropdownMenu.contains(eventTarget)) {
-
-            this.setState(
-                () => {
-                    return {
-                        showMenu: false
-                    };
-                },
-                () => {
-                    document.removeEventListener("click", this.closeMenu);
-                    document.removeEventListener("scroll", this.closeMenu);
-
-                }
-            )
-        }
-    };
-
-    onClickHandler = (item) => {
-        if(this.state.value !== item){
-            this.setState({
-                value: item
-            })
-        }
-        this.closeMenu();
-
-        if(this.props["onDropdownChangeHandler"]){
-            this.props["onDropdownChangeHandler"](item.value);
-        }
-    };
-
-    render(){
-
-        return (
-            <div className={"dropdown-wrap"}>
-                <button onClick={this.showBtnHandler} className={"btn dropdown-btn"}>
-                    <span className={"dropdown-value-name"}>{this.state.value.name}</span>
-                    <span className="caret"/>
-                </button>
-                {
-                    this.state.showMenu &&
-                    <div className="dropdown-menu"
-                        ref={(element) => {
-                            this.dropdownMenu = element;
-                        }}>
-
-                        {
-                            this.props["dropdownArrItems"].map((item, index) => {
-                                return(
-                                    <DropdownCardItem
-                                        key={index}
-                                        item={item}
-                                        onClickHandler={this.onClickHandler}
-                                    />
-                                )
-                            })
-                        }
-                    </div>
-                }
-            </div>
-        )
+        document.addEventListener("click", closeMenu);
+        document.addEventListener("scroll", closeMenu);
     }
+
+    function closeMenu(e, target){
+        const eventTarget = (e ? e.target : target);
+        if (dropdownMenu && !dropdownMenu.contains(eventTarget)) {
+            setShowMenu(false);
+            document.removeEventListener("click", closeMenu);
+            document.removeEventListener("scroll", closeMenu);
+        }
+    }
+
+    function onClickHandler(item){
+        if(value !== item){
+            setValue(item)
+        }
+
+        closeMenu();
+        if(props["onDropdownChangeHandler"]){
+            props["onDropdownChangeHandler"](item.value);
+        }
+    }
+
+
+    return (
+        <div className={"dropdown-wrap"}>
+            <button onClick={showBtnHandler} className={"btn dropdown-btn"}>
+                <span className={"dropdown-value-name"}>{value.name}</span>
+                <span className="caret"/>
+            </button>
+            {
+                showMenu &&
+                <div className="dropdown-menu"
+                    ref={(element) => {
+                        dropdownMenu = element;
+                    }}>
+
+                    {
+                        props["dropdownArrItems"].map((item, index) => {
+                            return(
+                                <DropdownCardItem
+                                    key={index}
+                                    item={item}
+                                    onClickHandler={onClickHandler}/>
+                            )
+                        })
+                    }
+                </div>
+            }
+        </div>
+    )
 };
