2c824f86eabcfc8b4ad439ea4a7aa905864c749e
maxim_novikov_88 <m.v.novikov@gmail.com>
2019-01-28 11:59:54 +0300

    fix active list state

--- src/js/components/common/layout/ActivitiesList.js
+++ src/js/components/common/layout/ActivitiesList.js
@@ -4,21 +4,10 @@ import { StickyContainer, Sticky } from "react-sticky";
 
 export default function ActivitiesList({activities, removeHandler}){
 
-  const [activitiesArr, setActivitiesArr] = useState(activities.all); //it's used by handleSortActivities
-  const [sortActivity, setSortActivity] = useState("");
+  const [sortActivity, setSortActivity] = useState("type");
 
-  useEffect(() => {
-    setActivitiesArr(activities.all);
-  }, [activities]);
-  
   function handleSortActivities(type) {
-    const sortedActivityArr = activitiesArr.sort((a, b) => {
-      if (a[type] < b[type]) return -1;
-      if (a[type] > b[type]) return 1;
-      return 0;
-    });
-    setActivitiesArr(sortedActivityArr);
-    setSortActivity(type)
+    setSortActivity(type);
   }
 
   return (
@@ -32,7 +21,7 @@ export default function ActivitiesList({activities, removeHandler}){
             return (
               <div className={"sticky-table-head" + (isSticky ? " sticky" : "")} style={style}>
                 <div className={"list-title row"}>
-                  <div className={"title"}>Title</div>
+                  <div className={"sorted title " + (sortActivity === "title" ? "active" : "")}>Title</div>
 
                   <div className={"sorted accessibility " + (sortActivity === "accessibility" ? "active" : "")}
                        onClick={() => handleSortActivities("accessibility")}>Accessibility <span/></div>
@@ -43,8 +32,8 @@ export default function ActivitiesList({activities, removeHandler}){
                   <div className={"sorted price " + (sortActivity === "price" ? "active" : "")}
                        onClick={() => handleSortActivities("price")}>Price <span/></div>
 
-                  <div className={"type"}
-                       onClick={() => handleSortActivities("type")}>Type</div>
+                  <div className={"sorted type " + (sortActivity === "type" ? "active" : "")}
+                       onClick={() => handleSortActivities("type")}>Type <span/></div>
                   <div className={"options"}/>
                 </div>
               </div>
@@ -54,7 +43,11 @@ export default function ActivitiesList({activities, removeHandler}){
 
         <ul className={"activities-list"}>
           {
-            activitiesArr.map((activity, i) => {
+            activities.all.sort((a, b) => {
+              if (a[sortActivity] < b[sortActivity]) return -1;
+              if (a[sortActivity] > b[sortActivity]) return 1;
+              return 0;
+            }).map((activity, i) => {
               return (
                 <li key={i} className={"activity-item row"}>
                   <div className={"title "}>{activity.activity}</div>
