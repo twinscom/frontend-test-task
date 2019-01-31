3883f96db1c50689271283362fa1144facf690fc
Anton Romankov <romant094@gmail.com>
2019-01-31 03:37:56 +0300

    Added page with random activity

--- src/App.js
+++ src/App.js
@@ -1,47 +1,99 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Activities from './components/Activities';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
-const BoredComponent = () => (
-	<>
-		<h1>This is a bored component</h1>
-	</>
-)
-const Nav = styled.nav`
+const settings = {
+	colors: {
+		grey: '#3F4045',
+		blue: '#5BC0BE',
+		darkBlue: '#0B132B',
+		white: '#F4F4F8',
+		yellow: '#FED766'
+	},
+	font: `'Montserrat', sans-serif`
+}
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+  html, body {
+    font-family: ${settings.font};
+  }
+  strong{
+	font-weight: 700;
+	}
 
 `
 
-const Ul = styled.ul`
+const Nav = styled.nav`
+background-color: ${settings.colors.grey};
+`
 
+const Container = styled.div`
+width: 70%;
+margin: 0 auto;
+padding: 20px 0;
+`
+
+const Ul = styled.ul`
+display: flex;
 
 `
 
 const Li = styled.li`
+padding: 0 20px;
+&:first-child{
+	padding-left: 0;
+}
+&:last-child{
+	padding-right: 0;
+}
+&:not(:last-child){
+	border-right: 1px dotted #F4F4F8;
+}
+a{
+	color: ${settings.colors.white};
+	transition: all .3s;
+	font-weight: 700;
 
-
+	&:hover{
+		color: ${settings.colors.blue};
+	}
+}
 `
 
 const Main = styled.main`
 
 `
 
+
+const BoredComponent = () => (
+	<>
+		<h1>This is a bored component</h1>
+	</>
+)
+
 const App = () => (
 	<Router>
 		<>
+			<GlobalStyle />
 			<Nav>
-				<Ul>
-					<Li>
-						<Link to="/">Home</Link>
-					</Li>
-					<Li>
-						<Link to="/bored/">About</Link>
-					</Li>
-				</Ul>
+				<Container>
+					<Ul>
+						<Li>
+							<Link to="/">Random activity</Link>
+						</Li>
+						<Li>
+							<Link to="/bored/">Bored</Link>
+						</Li>
+					</Ul>
+				</Container>
 			</Nav>
 			<Main>
-				<Route path="/" exact component={Activities} />
-				<Route path="/bored/" component={BoredComponent} />
+				<Container>
+					<Route path="/" exact render={() => <Activities settings={settings} />} />
+					<Route path="/bored/" component={BoredComponent} />
+				</Container>
 			</Main>
 		</>
 	</Router>
--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,32 +1,100 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
-function Activities() {
+const ActivityWrap = styled.div`
+border-radius: 20px;
+padding: 20px;
+width: 50%;
+margin: 0 auto;
+div{
+    margin-bottom: 10px;
+}
+`
+const ActivityHeader = styled.h3`
+text-align: center;
+font-size: 24px;
+font-weight: 700;
+line-height: 30px;
+margin-bottom: 5px;
+`
+const Preloader = styled.div`
+display: flex;
+justify-content: center;
+`
+const Participants = styled.div`
+display: flex;
+justify-content: space-between;
+`
+const Key = styled.div`
+display: flex;
+justify-content: flex-end;
+font-size: 12px;
+`
+const Type = styled.div`
 
+`
+const PriceWrap = styled.div`
+text-align: center;
+`
+const Price = styled.span`
+font-size: 24px;
+`
+
+function Activities(props) {
     const [data, setData] = useState({});
 
-    async function getActivity(result) {
-        result = await axios(
-            'http://www.boredapi.com/api/activity/'
-        );
-        setData(result.data);
+    function randomActivity() {
+        async function get() {
+            const result = await axios(
+                'http://www.boredapi.com/api/activity/'
+            );
+            setData(result.data);
+        }
+
+        useEffect(() => {
+            get(data);
+        }, []);
     }
+    randomActivity();
 
-    useEffect(() => {
-        getActivity(data);
-    },[]);
+    const { activity, key, participants, price, type } = data;
 
-    const { accessibility, activity, key, participants, price, type } = data;
-    
-    return (
-        <div>
-            <p>accessibility: {accessibility}</p>
-            <p>activity: {activity}</p>
-            <p>key: {key}</p>
-            <p>participants: {participants}</p>
-            <p>price: {price}</p>
-            <p>type: {type}</p>
-        </div>
-    );
+    if (Object.keys(data).length === 0) {
+        return (
+            <Preloader>
+                <img src="img/25.gif" alt="Loading..." />
+            </Preloader>
+        )
+    } else {
+        return (
+            <>
+                <ActivityWrap style={{ backgroundColor: props.settings.colors.grey, color: props.settings.colors.white }}>
+                    <ActivityHeader>{activity}</ActivityHeader>
+                    <Key>
+                        id: {key}
+                    </Key>
+                    <Type>
+                        <span>Activity type: <strong style={{ color: props.settings.colors.blue }}>{type}</strong></span>
+                    </Type>
+                    <Participants>
+                        Participants: {participants}
+                    </Participants>                    
+                    <PriceWrap>
+                        <Price>
+                            Price:&nbsp;
+                            <i style={price === 0 ? { textTransform: 'uppercase', color: props.settings.colors.yellow, fontWeight: 700 } : {}}>
+                                {price === 0 ? 'free' : `$${price}`}
+                            </i>
+                        </Price>
+                    </PriceWrap>
+
+                </ActivityWrap>
+
+                {/* <button onClick={randomActivity}>Random</button> */}
+            </>
+        )
+    }
 }
+
 export default Activities;
